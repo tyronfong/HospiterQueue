c60a19af13e3f7630ce5e6bc6b82eefcbfd65c7d
Tyron Fong <tyronfong@outlook.com>
2016-04-10 12:46:08 +0800

    add logout function

--- tyron/src/com/tyron/signon/controller/SignonController.java
+++ tyron/src/com/tyron/signon/controller/SignonController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,5 +32,18 @@ public class SignonController {
 		}
 
 		return new ModelAndView(new RedirectView(request.getContextPath()));
+	}
+	
+	@RequestMapping("dologout.op")
+	public ModelAndView doLogout()
+	{
+		Subject subject = SecurityUtils.getSubject();
+		ModelAndView signonPage = new ModelAndView("/signon/signon");
+		if(subject.isAuthenticated())
+		{
+			subject.logout();
+		}
+		
+		return signonPage;
 	}
 }
